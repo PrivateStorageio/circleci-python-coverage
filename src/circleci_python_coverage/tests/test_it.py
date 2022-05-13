e31f2280a6c8b48adabe573e027718142256c992
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-05-13 18:32:39 -0400

    make it easy to see which platforms we're succeeding at

--- src/circleci_python_coverage/tests/test_it.py
+++ src/circleci_python_coverage/tests/test_it.py
@@ -1,7 +1,18 @@
-from unittest import TestCase
+from unittest import TestCase, skipUnless
+from sys import platform
 
-from circleci_python_coverage import foo
+from circleci_python_coverage import linux, windows, macos
 
 class Tests(TestCase):
-    def test_foo(self):
-        foo()
+
+    @skipUnless("linux" in platform, "")
+    def test_linux(self):
+        linux.foo()
+
+    @skipUnless("win32" in platform, "")
+    def test_windows(self):
+        windows.foo()
+
+    @skipUnless("darwin" in platform, "")
+    def test_macos(self):
+        macos.foo()
