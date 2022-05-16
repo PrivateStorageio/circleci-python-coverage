dc95d14c7033f17413533597390c8bf2860452e1
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-05-16 08:15:16 -0400

    "Can't combine arc data with line data"
    Maybe this means some runs use --branch and some don't?
    
    Look at the config for each run.

--- tests.nix
+++ tests.nix
@@ -30,7 +30,7 @@ let
   tests = pkgs.runCommand "circleci-python-coverage" {
   } ''
     set -x
-    ${python}/bin/python -m coverage run -m unittest discover --verbose circleci_python_coverage.tests
+    ${python}/bin/python -m coverage run --debug=config -m unittest discover --verbose circleci_python_coverage.tests
     SOURCE=$(${python}/bin/python -c 'import circleci_python_coverage; print(circleci_python_coverage.__file__.rsplit("/", 1)[0])')
     ${python}/bin/python -m slipcover --source $SOURCE --json --pretty-print --out slipcover.json -m unittest discover --verbose circleci_python_coverage.tests
 
